56d135b83fdbffddd0f4024b34864901878ecd83
Keith Brown <s.keith.brown@gmail.com>
2016-01-15 16:56:09 -0500

    job #8066 - created new plugin org.xtuml.bp.masl.model to provide BP in-memory access to the masl model.  Just getting it bootstrapped. Still working through problems.

---
+++ src/org.xtuml.bp.masl.model/src/org/xtuml/bp/masl/model/MASLModelPlugin.java
@@ -0,0 +1,69 @@
+package org.xtuml.bp.masl.model;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+import org.xtuml.bp.core.util.CoreUtil;
+
+public class MASLModelPlugin extends AbstractUIPlugin {
+	private static MASLModelPlugin plugin;
+	private ResourceBundle resourceBundle;
+	private static boolean isActivated;
+
+	public MASLModelPlugin() {
+		super();
+		plugin = this;
+		try {
+			resourceBundle = ResourceBundle.getBundle("org.xtuml.bp.masl.model.MASLModelPluginResources"); //$NON-NLS-1$
+		} catch (MissingResourceException x) {
+			resourceBundle = null;
+		}
+	}
+
+	public static MASLModelPlugin getDefault() {
+		return plugin;
+	}
+
+	public static IWorkspace getWorkspace() {
+		return ResourcesPlugin.getWorkspace();
+	}
+
+	public static String getResourceString(String key) {
+		ResourceBundle bundle = MASLModelPlugin.getDefault().getResourceBundle();
+		try {
+			return bundle.getString(key);
+		} catch (MissingResourceException e) {
+			return key;
+		}
+	}
+
+	public ResourceBundle getResourceBundle() {
+		return resourceBundle;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		if (!CoreUtil.IsRunningHeadless) {
+			super.start(context);
+			MASLModelPlugin.isActivated = true;
+		} else {
+			MASLModelPlugin.isActivated = false;
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		if (MASLModelPlugin.isActivated) {
+			super.stop(context);
+		}
+	}
+
+}
