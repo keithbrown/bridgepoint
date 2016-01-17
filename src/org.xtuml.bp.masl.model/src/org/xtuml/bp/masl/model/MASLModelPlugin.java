dcddbecac6370696eb9348b7c467c89d900f283f
Keith Brown <s.keith.brown@gmail.com>
2016-01-16 20:10:14 -0500

    job #8066 - more tweaks on the example to have something simple that builds.

--- src/org.xtuml.bp.masl.model/src/org/xtuml/bp/masl/model/MASLModelPlugin.java
+++ src/org.xtuml.bp.masl.model/src/org/xtuml/bp/masl/model/MASLModelPlugin.java
@@ -5,8 +5,11 @@ import java.util.ResourceBundle;
 
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
 
 public class MASLModelPlugin extends AbstractUIPlugin {
 	private static MASLModelPlugin plugin;
@@ -42,6 +45,21 @@ public class MASLModelPlugin extends AbstractUIPlugin {
 
 	public ResourceBundle getResourceBundle() {
 		return resourceBundle;
+	}
+
+	public static void logError(String msg, Throwable e) {
+		MASLModelPlugin cp = getDefault();
+		// plugin is null when running unit tests
+		if (cp != null) {
+			Status status = new Status(IStatus.ERROR, (String) cp.getBundle().getHeaders().get(Constants.BUNDLE_NAME),
+					IStatus.ERROR, msg, e);
+			getDefault().getLog().log(status);
+		} else {
+			System.err.println(msg);
+			if (e != null) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	/* (non-Javadoc)
